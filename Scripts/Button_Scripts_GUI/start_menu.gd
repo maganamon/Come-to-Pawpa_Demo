e4cbbdce979baca9c309f86beb078a52d5eb4d84
MaganaMon <rodolfomagana8@gmail.com>
2024-08-16 17:47:05 -0700

    changed wait time on button press

--- Scripts/Button_Scripts_GUI/start_menu.gd
+++ Scripts/Button_Scripts_GUI/start_menu.gd
@@ -2,13 +2,13 @@ extends Control
 
 
 func _on_play_button_pressed():
-	$menu_timer.start(1.0)
+	$menu_timer.start(0.75)
 	await $menu_timer.timeout
 	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
 
 
 func _on_exit_button_pressed():
-	$menu_timer.start(1.0)
+	$menu_timer.start(0.75)
 	await $menu_timer.timeout
 	get_tree().quit()
 	pass # Replace with function body.
