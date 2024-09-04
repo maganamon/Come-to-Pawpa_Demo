72d6ce3489d6ecb1ac3675723fabd9ed658e3b47
TheUniverseEquation <161095013+TheUniverseEquation@users.noreply.github.com>
2024-09-03 23:25:50 -0700

    added working hex buttons.
    Buttons take to level 1 but will be redirected to level 2 later on. Will also be updating the card designs as well.

--- Scripts/Menu_Scripts/hex_transition.gd
+++ Scripts/Menu_Scripts/hex_transition.gd
@@ -1,16 +1,17 @@
 extends Node2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$AnimatedSprite2D.play("RepeatMe")
+##	$AnimatedSprite2D.play("RepeatMe")
 
-func _on_hex_transition_rabie_infested_mutts_button_pressed():
-	print("rabies")
-
-
-func _on_hex_transition_healing_hounds_button_pressed():
-	print("healing")
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_button_pressed() -> void:
+	print(GlobalScript.dogHealth)
+	GlobalScript.dogHealth = 3
+	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
+
+
+func _on_hex_transitionrabie_infested_mutts_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
