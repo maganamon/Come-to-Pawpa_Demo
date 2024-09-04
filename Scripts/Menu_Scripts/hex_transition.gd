d841bf3172b3a9198c2a705bb1c096b420bec021
TheUniverseEquation <161095013+TheUniverseEquation@users.noreply.github.com>
2024-09-03 22:03:58 -0700

    adding hex cards to hex transition scene

--- Scripts/Menu_Scripts/hex_transition.gd
+++ Scripts/Menu_Scripts/hex_transition.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimatedSprite2D.play("RepeatMe")
 
+func _on_hex_transition_rabie_infested_mutts_button_pressed():
+	print("rabies")
 
+
+func _on_hex_transition_healing_hounds_button_pressed():
+	print("healing")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
