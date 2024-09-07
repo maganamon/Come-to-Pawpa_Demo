5b5fc1fb32bb32f3b2713a4c651aca2713b2de13
maganamon <113212564+maganamon@users.noreply.github.com>
2024-09-07 02:23:17 -0700

    Hex Scene goes to Level 2 scene

--- Scripts/Menu_Scripts/hex_transition.gd
+++ Scripts/Menu_Scripts/hex_transition.gd
@@ -12,12 +12,12 @@ func _on_hex_transition_roided_mutts_button_pressed() -> void:
 	print(GlobalScript.dogHealth)
 	GlobalScript.dogHealth = 3
 	print(GlobalScript.dogHealth)
-	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_two_scene.tscn")
 
 
 func _on_hex_transition_rabid_hounds_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_two_scene.tscn")
 
 
 func _on_hex_transition_pussy_card_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_one.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level_Scenes/level_two_scene.tscn")
