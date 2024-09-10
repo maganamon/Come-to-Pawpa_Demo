75d6ecb4d16efe8231fc2c1851e8a645218bf0e1
maganamon <113212564+maganamon@users.noreply.github.com>
2024-09-09 23:27:52 -0700

    Revert "testing local push"
    This reverts commit 3962b3e24df572b8bdd529589107b8f62843ae5e.

--- Scripts/GUI_Scripts/time_left_2d_sprite.gd
+++ Scripts/GUI_Scripts/time_left_2d_sprite.gd
@@ -3,21 +3,15 @@ extends Sprite2D
 var display_me
 var purp_txt
 var white_txt
-var timeToDisplay
-var timer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer = $"Time_left-Round_timer"
-	timeToDisplay = GlobalScript._giveMe_LevelTime()
-	print(timeToDisplay)
-	timer.start(timeToDisplay)
 	GlobalScript.player_died.connect(_on_player_died)
 	purp_txt = $"Time_Left-Number-Label_purple"
 	white_txt = $"Time_Left-Number-Label_white"
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	purp_txt.text = str(ceil(timer.time_left))
-	white_txt.text = str(ceil(timer.time_left))
+	purp_txt.text = str(ceil($"Time_left-Round_timer".time_left))
+	white_txt.text = str(ceil($"Time_left-Round_timer".time_left))
 
 func _on_player_died():
-	timer.paused = true
+	$"Time_left-Round_timer".paused = true
